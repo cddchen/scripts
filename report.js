526880165578ce16d24bdc473a591a1afd185556
cddchen <31310935+cddchen@users.noreply.github.com>
2022-04-28 21:15:34 +0800

    Create report.js

---
+++ report.js
@@ -0,0 +1,7 @@
+var obj = JSON.parse($response.body);
+obj.entities.create = 1651151644
+obj.entities.remarks[0].assignTime = 1651151644
+
+obj.entities.remarks[1].assignTime = 1651151644
+
+$done({body: JSON.stringify(obj)});
