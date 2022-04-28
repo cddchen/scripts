45602e5f12b82da56e964134748d2625e5dc8b00
cddchen <31310935+cddchen@users.noreply.github.com>
2022-04-28 21:20:56 +0800

    Update report.js

--- report.js
+++ report.js
@@ -1,7 +1,8 @@
 var obj = JSON.parse($response.body);
 obj.entities.create = 1651151644
-obj.entities.remarks[0].assignTime = 1651151644
+obj.entities.name = "YSML"
+// obj.entities.remarks[0].assignTime = 1651151644
 
-obj.entities.remarks[1].assignTime = 1651151644
+// obj.entities.remarks[1].assignTime = 1651151644
 
 $done({body: JSON.stringify(obj)});
