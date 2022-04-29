5809ababbc57a897763dcae3a172b11b522ff520
cddchen <31310935+cddchen@users.noreply.github.com>
2022-04-29 10:36:05 +0800

    Update report.js

--- report.js
+++ report.js
@@ -6,6 +6,7 @@ obj.entities[0].step.assignTime = 1651218681
 obj.entities[0].step.actionTime = 1651218681
 obj.entities[0].step.renderTime = 1651218681
 
+obj.entities[0].data.fieldSHZT = "2022-04-29"
 obj.entities[0].data.fieldSQSJ = 1651218681
 obj.entities[0].data.fieldSHFDYShsj = 1651161600
 obj.entities[0].data._VAR_TODAY = 1651161600
@@ -14,9 +15,9 @@ obj.entities[0].data.fieldSQJCRQ = 1651161600
 //申请时间
 obj.entities[0].fields.fieldSQSJ.initialValue = 1651218681
 //辅导员审核时间
-obj.entities[0].fields.fieldSHFDYShsj.initialValue = 1651218681
+obj.entities[0].fields.fieldSHFDYShsj.initialValue = 1651218681+180
 //副书记审核时间
-obj.entities[0].fields.fieldSHSZXYShsj.initialValue = 1651218681
+obj.entities[0].fields.fieldSHSZXYShsj.initialValue = 1651218681+180
 
 
 $done({body: JSON.stringify(obj)});
