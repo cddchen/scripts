38f0725c05672ff613185e3416e21cc4991a580e
cddchen <31310935+cddchen@users.noreply.github.com>
2022-04-29 09:40:19 +0800

    Update report.js

--- report.js
+++ report.js
@@ -1,4 +1,5 @@
+//http:\/\/task.njust.edu.cn\/infoplus\/interface\/render
 var obj = JSON.parse($response.body);
-obj.entities.app.name = "YSML"
+obj.entities[0].app.name = "YSML"
 
 $done({body: JSON.stringify(obj)});
