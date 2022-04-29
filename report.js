8b23293a17f013bd79f0a8f20e45beb8fa24c2f3
cddchen <31310935+cddchen@users.noreply.github.com>
2022-04-29 09:52:29 +0800

    Update report.js

--- report.js
+++ report.js
@@ -1,8 +1,10 @@
 //http:\/\/task.njust.edu.cn\/infoplus\/interface\/render
 const referer = `http://task.njust.edu.cn/infoplus/form/14478876/render`
-if ($request.referer == referer) {
-	var obj = JSON.parse($response.body);
-	obj.entities[0].app.name = "YSML"
+var obj = JSON.parse($response.body);
+// my
+obj.entities[0].step.assignTime = 1651218681
+obj.entities[0].step.actionTime = 1651218681
+obj.entities[0].step.renderTime = 1651218681
 
-	$done({body: JSON.stringify(obj)});
-}
+
+$done({body: JSON.stringify(obj)});
