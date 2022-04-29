579250ae57b6913f839a618bce689c568a80836d
cddchen <31310935+cddchen@users.noreply.github.com>
2022-04-29 09:45:01 +0800

    Update report.js

--- report.js
+++ report.js
@@ -1,5 +1,8 @@
 //http:\/\/task.njust.edu.cn\/infoplus\/interface\/render
-var obj = JSON.parse($response.body);
-obj.entities[0].app.name = "YSML"
+const referer = `http://task.njust.edu.cn/infoplus/form/14478876/render`
+if ($request.referer == referer) {
+	var obj = JSON.parse($response.body);
+	obj.entities[0].app.name = "YSML"
 
-$done({body: JSON.stringify(obj)});
+	$done({body: JSON.stringify(obj)});
+}
