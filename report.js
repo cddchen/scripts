4f0fe9d679360548c39f013c03c1da0e93031b11
cddchen <31310935+cddchen@users.noreply.github.com>
2022-04-29 10:08:55 +0800

    Update report.js

--- report.js
+++ report.js
@@ -2,9 +2,9 @@
 const referer = `http://task.njust.edu.cn/infoplus/form/14478876/render`
 var obj = JSON.parse($response.body);
 // my
-// obj.entities[0].step.assignTime = 1651218681
-// obj.entities[0].step.actionTime = 1651218681
-// obj.entities[0].step.renderTime = 1651218681
+obj.entities[0].step.assignTime = 1651218681
+obj.entities[0].step.actionTime = 1651218681
+obj.entities[0].step.renderTime = 1651218681
 
 obj.entities[0].data.fieldSQSJ = 1651218681
 obj.entities[0].data.fieldSHFDYShsj = 1651161600
