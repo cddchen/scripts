94c982d4f979601345921a3e8749d7113e1c817a
cddchen <31310935+cddchen@users.noreply.github.com>
2022-04-29 10:34:13 +0800

    Update report_detail.js

--- report_detail.js
+++ report_detail.js
@@ -2,16 +2,11 @@
 const referer = `http://task.njust.edu.cn/infoplus/form/14478876/render`
 var obj = JSON.parse($response.body);
 
-// obj.entities[0].data.fieldSQSJ = 1651218681
-// obj.entities[0].data.fieldSHFDYShsj = 1651161600
-// obj.entities[0].data._VAR_TODAY = 1651161600
-// obj.entities[0].data.fieldSQJCRQ = 1651161600
-
 obj.entities[0].create = 1651218681
 //辅导员
-obj.entities[0].remarks[0].assignTime = 1651218681
-obj.entities[0].remarks[0].actionTime = 1651218681
-obj.entities[0].remarks[0].renderTime = 1651218681
+obj.entities[0].remarks[0].assignTime = 1651218681+180
+obj.entities[0].remarks[0].actionTime = 1651218681+180
+obj.entities[0].remarks[0].renderTime = 1651218681+180
 //申请人
 obj.entities[0].remarks[1].assignTime = 1651218681
 obj.entities[0].remarks[1].actionTime = 1651218681
