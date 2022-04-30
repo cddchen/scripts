f27e874bc00b7d89e2d637af90247c50e533c8fa
cddchen <31310935+cddchen@users.noreply.github.com>
2022-04-30 11:51:24 +0800

    Update report_detail.js

--- report_detail.js
+++ report_detail.js
@@ -1,16 +1,18 @@
 //http:\/\/task.njust.edu.cn\/infoplus\/interface\/render
 const referer = `http://task.njust.edu.cn/infoplus/form/14478876/render`
 var obj = JSON.parse($response.body);
+var now = new Date();
+now.setHours(15, 43)
+const time = now.getTime()/1000
 
-obj.entities[0].create = 1651218681
+obj.entities[0].create = time
 //辅导员
-obj.entities[0].remarks[0].assignTime = 1651218681+180
-obj.entities[0].remarks[0].actionTime = 1651218681+180
-obj.entities[0].remarks[0].renderTime = 1651218681+180
+obj.entities[0].remarks[0].assignTime = time+180
+obj.entities[0].remarks[0].actionTime = time+180
+obj.entities[0].remarks[0].renderTime = time+180
 //申请人
-obj.entities[0].remarks[1].assignTime = 1651218681
-obj.entities[0].remarks[1].actionTime = 1651218681
-obj.entities[0].remarks[1].renderTime = 1651218681
-
+obj.entities[0].remarks[1].assignTime = time
+obj.entities[0].remarks[1].actionTime = time
+obj.entities[0].remarks[1].renderTime = time
 
 $done({body: JSON.stringify(obj)});
