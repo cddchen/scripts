db87b53420420189d61c5eb28a36044b039a90a9
cddchen <31310935+cddchen@users.noreply.github.com>
2022-05-23 11:15:42 +0800

    Update XBOXCLUB.js

--- XBOXCLUB.js
+++ XBOXCLUB.js
@@ -15,7 +15,7 @@ function getHeaders() {
   if ($request) {
     const signurlVal = $request.url
     const signheaderVal = JSON.stringify($request.headers)
-    const signbodyVal = $request.body
+    const signbodyVal = JSON.stringify($request.body)
 
     if (signurlVal) $.setData(signurlVal, $.signurlKey)
     if (signheaderVal) $.setData(signheaderVal, $.signheaderKey)
@@ -37,7 +37,7 @@ function sign() {
   $.get(url, (error, response, data) => {
     $.log(`${$.name}, data: ${data}`)
     let result = JSON.parse(data)
-    let subTitle = `结果：${result.msg}`
+    let subTitle = `结果：${result.msg}，${result.data.desc}`
     $.msg($.name, subTitle)
     $.done()
   })
