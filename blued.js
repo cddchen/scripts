b6f49a483ac0967634bdef94a3b800c6bb6e05c2
cddchen <31310935+cddchen@users.noreply.github.com>
2022-06-03 23:20:50 +0800

    Update blued.js

--- blued.js
+++ blued.js
@@ -2,14 +2,19 @@ var objc = JSON.parse($response.body);
     
 objc.data[0].is_hidden_special_gift = 1
 objc.data[0].is_filter_ads = 1
-objc.data[0].vip_grade = 1
+objc.data[0].vip_grade = 2
 objc.data[0].is_vip_annual = 1
 objc.data[0].live_tabs = []
 for (var obj in objc.data[0].vip_split) {
     objc.data[0].vip_split[obj] = 1
 }
 objc.data[0].hide_vip_price = 1
-this.log(objc)
+objc.data[0].chat_filter_duration = 45
+objc.data[0].ad_close_pop = 0
+objc.data[0].nearby_recommend_live_ui = 0
+objc.data[0].is_nearby_live_play_disable = 1
+
+console.log(objc)
 $done({
     body : JSON.stringify(objc)
 });
