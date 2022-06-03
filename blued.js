a34a4fcbd7b88465e84b55b149e1d1e63450f7c7
cddchen <31310935+cddchen@users.noreply.github.com>
2022-06-03 22:25:29 +0800

    Create blued.js

---
+++ blued.js
@@ -0,0 +1,16 @@
+var objc = JSON.parse($response.body);
+    
+objc.data[0].is_hidden_special_gift = 1
+objc.data[0].is_filter_ads = 1
+objc.data[0].vip_grade = 1
+objc.data[0].is_vip_annual = 1
+objc.data[0].live_tabs = []
+for (var obj in objc.data[0].vip_split) {
+    objc.data[0].vip_split[obj] = 1
+}
+objc.data[0].tt_promotion.open = 0
+objc.data[0].hide_vip_price = 1
+
+$done({
+    body : JSON.stringify(objc)
+});
