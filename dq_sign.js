2933466dc886dc03887de3b0b6d4a714b5034d43
cddchen <31310935+cddchen@users.noreply.github.com>
2022-09-13 16:11:42 +0800

    Create dq_sign.js

---
+++ dq_sign.js
@@ -0,0 +1,268 @@
+/******************************
+[rewrite_local]
+https:\/\/wxxcx\.dairyqueen\.com\.cn\/UserXueLi\?_actionName=getXueLiSign url script-request-body https://raw.githubusercontent.com/cddchen/scripts/main/dq_sign.js
+
+[mitm] 
+hostname = *.dairyqueen.com.cn
+
+*******************************/
+
+const $ = new Env("DQ点单小程序签到");
+$.signKey = 'dq_session'
+
+const isRequest = () => typeof $request !== "undefined" && typeof $response === "undefined";
+const timestamp = new Date().getTime();
+const notify = $.isNode() ? require('./sendNotify') : '';
+$.subt = ''
+$.subtt = ''
+let cookieArr = []
+
+if (isRequest) {
+  !(async () => {
+    // $.log(JSON.stringify($request))
+    const session = {}
+    session.url = $request.url;
+    session.headers = $request.headers;
+    session.body = $request.body;
+    if ($.setJson(session), $.signKey) {
+      $.subt = `获取会话: 成功!`
+    } else {
+      $.subt = `获取会话: 失败!`
+    }
+    $.msg($.name, $.subt, '')
+    $.log(JSON.stringify(session))
+  })()
+    .catch((e) => $.logErr(e))
+    .finally(() => $.done())
+} else {
+  !(async () => {
+    if ($.isNode) {
+      getConfig();
+      for (let i = 0, len = cookieArr.length; i < len; i++) {
+        $.index = i + 1;
+        $.log(`开始第${$.index}个账号签到`)
+        try {
+          session = cookieArr[i]
+          const sign = await getXueLiSign(session);
+          await loginNoLandfall(session, sign);
+          await signIn(session);
+        } catch (e) {
+          $.logErr(e);
+        }
+      }
+    }
+    else {
+      const session = $.getData($.signKey);
+      const sign = await getXueLiSign(session);
+      await loginNoLandfall(session, sign);
+      await signIn(session);
+    }
+    if ($.isNode()) await notify.sendNotify(`${$.name}`, $.subt, {}, $.subtt);
+    else if (cookieArr.length) {
+      $.msg($.name, ``, $.subt, $.subtt);
+    }
+  })()
+    .catch((e) => $.logErr(e))
+    .finally(() => $.done())
+}
+
+function getConfig() {
+  if ($.isNode()) {
+    let cks = [];
+    if (process.env.DQ) {
+      if (process.env.DQ.indexOf('&') > -1) {
+        cks = process.env.DQ.split('&');
+      } else if (process.env.DQ.indexOf('\n') > -1) {
+        cks = process.env.DQ.split('\n');
+      } else {
+        cks = [process.env.DQ];
+      }
+    }
+    if (!cks.length) {
+      $.log(`\n请先设置Cookie`);
+    } else {
+      for (let i = 0, len = cks.length; i < len; i++) {
+        cookieArr.push(cks[i])
+      }
+    }
+  }
+}
+
+function getCookie() {
+  const regex = /^https:\/\/wxxcx\.dairyqueen\.com\.cn\/UserXueLi\?_actionName=getXueLiSign/;
+  if (regex.test($request.url)) {
+    const session = {}
+    session.url = $request.url;
+    session.headers = $request.headers;
+
+    if ($.setJson(session, $.signKey)) {
+      $.subt = `获取会话: 成功!`
+    } else {
+      $.subt = `获取会话: 失败!`
+    }
+    $.msg($.name, $.subt, '')
+    $.log(JSON.stringify(session))
+  }
+}
+
+function getXueLiSign(session) {
+  return new Promise((resolve, reject) => {
+    session = JSON.parse(session)
+    body = JSON.parse(session.body)
+    body.content.timestamp = timestamp;
+    const headers = {
+      "Accept-Language": "zh-cn",
+      "Content-Type": "application/json;charset=utf-8",
+      "User-Agent":
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.3(0x18000315) NetType/WIFI Language/zh_CN miniProgram",
+      Connection: "keep-alive",
+    };
+    const options = {
+      url: session.url,
+      headers: headers,
+      body: JSON.stringify(body)
+    };
+    $.post(options, (error, resp, data) => {
+      try {
+        if (error) {
+          $.logErr(error, resp);
+          $.subt += `${$.index}：getXueLiSign结果: 失败\n`
+          $.subtt += `${$.index}：${error}\n`
+          // $.msg($.name, "getXueLiSign结果: 失败", error);
+        } else if (resp.status === 200) {
+          const result = JSON.parse(data);
+          if (result.status === 1) {
+            subTitle = `签到结果: 成功`;
+            $.subt += `${$.index}：签到结果: 成功\n`
+            if (result.data && result.data.sign) {
+              resolve(result.data.sign);
+            } else {
+              subTitle = `getXueLiSign结果: 失败`;
+              detail = `详情参见日志`;
+              $.subt += `${$.index}：` + subTitle + `\n`
+              $.subtt += `${$.index}：` + detail + `\n`
+              throw new Error(JSON.stringify(resp));
+            }
+          } else {
+            subTitle = `getXueLiSign结果: 失败`;
+            detail = `${result.message}`;
+            // $.msg($.name, subTitle, detail);
+            $.subt += `${$.index}：` + subTitle + `\n`
+            $.subtt += `${$.index}：` + detail + `\n`
+            throw new Error(result.message);
+          }
+        }
+      } catch (error) {
+        $.logErr(error, resp);
+        reject(error);
+      }
+    });
+  })
+}
+
+function loginNoLandfall(session, sign) {
+  return new Promise((resolve, reject) => {
+    session = JSON.parse(session)
+    body = JSON.parse(session.body).content
+    body.timestamp = timestamp;
+    body.sign = sign
+    const headers = {
+      Host: "wechat.dairyqueen.com.cn",
+      channel: `${body.channelId}`,
+      tenant: `${body.tenantId}`,
+      "Accept-Language": "zh-cn",
+      "Content-Type": "application/json;charset=utf-8",
+      Origin: "https://wechat.dairyqueen.com.cn",
+      "User-Agent":
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.3(0x18000315) NetType/WIFI Language/zh_CN miniProgram",
+      Connection: "keep-alive",
+    };
+    const options = {
+      url: "https://wechat.dairyqueen.com.cn/loginNoLandfall",
+      headers: headers,
+      body: JSON.stringify(body),
+    };
+    $.post(options, (error, resp, data) => {
+      try {
+        if (error) {
+          $.logErr(error, resp);
+          $.subt += `${$.index}：loginNoLandfall结果: 失败\n`
+          $.subtt += `${$.index}：${error}\n`
+          // $.msg($.name, "getXueLiSign结果: 失败", error);
+        } else if (resp.status === 200) {
+          const result = JSON.parse(data);
+          if (result.message === "success") {
+            resolve(JSON.stringify(resp));
+          } else {
+            subTitle = `loginNoLandfall结果: 失败`;
+            $.log("", result.message);
+            detail = `${result.message}`;
+            // $.msg($.name, subTitle, detail);
+            $.subt += `${$.index}：` + subTitle + `\n`
+            $.subtt += `${$.index}：` + detail + `\n`
+            throw new Error(result.message);
+          }
+        }
+      } catch (error) {
+        $.logErr(error, resp);
+        reject(error);
+      }
+    });
+  })
+}
+
+function signIn(session) {
+  return new Promise((resolve, reject) => {
+    session = JSON.parse(session)
+    body = JSON.parse(session.body).content
+    const headers = {
+      Host: "wechat.dairyqueen.com.cn",
+      channel: `${body.channelId}`,
+      tenant: `${body.tenantId}`,
+      "Accept-Language": "zh-cn",
+      "Content-Type": "application/json;charset=utf-8",
+      Origin: "https://wechat.dairyqueen.com.cn",
+      "User-Agent":
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.3(0x18000315) NetType/WIFI Language/zh_CN miniProgram",
+      Connection: "keep-alive",
+    };
+    const options = {
+      url: "https://wechat.dairyqueen.com.cn/memSignIn/signIn",
+      headers: headers,
+      body: {},
+    };
+    $.post(options, (error, resp, data) => {
+      try {
+        if (error) {
+          $.logErr(error, resp);
+          $.subt += `${$.index}：签到结果: 失败\n`
+          $.subtt += `${$.index}：${error}\n`
+          // $.msg($.name, "getXueLiSign结果: 失败", error);
+        } else if (resp.status === 200) {
+          const result = JSON.parse(data);
+          if (result.message === "success") {
+            subTitle = `签到结果: 成功`;
+            detail = ``;
+          } else if (result.code === 11028) {
+            subTitle = `签到结果: 成功`;
+            detail = `${result.message}`;
+          } else {
+            subTitle = `签到结果: 失败`;
+            $.log("", result.message);
+            detail = `${result.message}`;
+          }
+          // $.msg($.name, subTitle, detail);
+          $.subt += `${$.index}：` + subTitle + `\n`
+          $.subtt += `${$.index}：` + detail + `\n`
+        }
+      } catch (error) {
+        $.logErr(error, resp);
+        reject(error);
+      }
+    });
+  })
+}
+
+/***************** Env *****************/
+// prettier-ignore
+function Env(t, e) { class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise((e, i) => { s.call(this, t, (t, s, r) => { t ? i(t) : e(s) }) }) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `\ud83d\udd14${this.name}, \u5f00\u59cb!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } isSurge() { return "undefined" != typeof $httpClient && "undefined" == typeof $loon } isLoon() { return "undefined" != typeof $loon } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; const i = this.getdata(t); if (i) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise(e => { this.get({ url: t }, (t, s, i) => e(i)) }) } runScript(t, e) { return new Promise(s => { let i = this.getdata("@chavy_boxjs_userCfgs.httpapi"); i = i ? i.replace(/\n/g, "").trim() : i; let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r; const [o, h] = i.split("@"), a = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, Accept: "*/*" } }; this.post(a, (t, e, i) => s(i)) }).catch(t => this.logErr(t)) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e); if (!s && !i) return {}; { const i = s ? t : e; try { return JSON.parse(this.fs.readFileSync(i)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r) } } lodash_get(t, e, s) { const i = e.replace(/\[(\d+)\]/g, ".$1").split("."); let r = t; for (const t of i) if (r = Object(r)[t], void 0 === r) return s; return r } lodash_set(t, e, s) { return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t) } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : ""; if (r) try { const t = JSON.parse(r); e = t ? this.lodash_get(t, i, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}"; try { const e = JSON.parse(h); this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i) } catch (e) { const o = {}; this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i) } } else s = this.setval(t, e); return s } getval(t) { return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null } setval(t, e) { return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } }).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, resp: i } = t; e(s, i, i && i.body) })) } post(t, e = (() => { })) { if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.post(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) }); else if (this.isQuanX()) t.method = "POST", this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t)); else if (this.isNode()) { this.initGotEnv(t); const { url: s, ...i } = t; this.got.post(s, i).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, resp: i } = t; e(s, i, i && i.body) }) } } time(t) { let e = { "M+": (new Date).getMonth() + 1, "d+": (new Date).getDate(), "H+": (new Date).getHours(), "m+": (new Date).getMinutes(), "s+": (new Date).getSeconds(), "q+": Math.floor(((new Date).getMonth() + 3) / 3), S: (new Date).getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, ((new Date).getFullYear() + "").substr(4 - RegExp.$1.length))); for (let s in e) new RegExp("(" + s + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? e[s] : ("00" + e[s]).substr(("" + e[s]).length))); return t } msg(e = t, s = "", i = "", r) { const o = t => { if (!t) return t; if ("string" == typeof t) return this.isLoon() ? t : this.isQuanX() ? { "open-url": t } : this.isSurge() ? { url: t } : void 0; if ("object" == typeof t) { if (this.isLoon()) { let e = t.openUrl || t.url || t["open-url"], s = t.mediaUrl || t["media-url"]; return { openUrl: e, mediaUrl: s } } if (this.isQuanX()) { let e = t["open-url"] || t.url || t.openUrl, s = t["media-url"] || t.mediaUrl; return { "open-url": e, "media-url": s } } if (this.isSurge()) { let e = t.url || t.openUrl || t["open-url"]; return { url: e } } } }; this.isMute || (this.isSurge() || this.isLoon() ? $notification.post(e, s, i, o(r)) : this.isQuanX() && $notify(e, s, i, o(r))); let h = ["", "==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="]; h.push(e), s && h.push(s), i && h.push(i), console.log(h.join("\n")), this.logs = this.logs.concat(h) } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { const s = !this.isSurge() && !this.isQuanX() && !this.isLoon(); s ? this.log("", `\u2757\ufe0f${this.name}, \u9519\u8bef!`, t.stack) : this.log("", `\u2757\ufe0f${this.name}, \u9519\u8bef!`, t) } wait(t) { return new Promise(e => setTimeout(e, t)) } done(t = {}) { const e = (new Date).getTime(), s = (e - this.startTime) / 1e3; this.log("", `\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`), this.log(), (this.isSurge() || this.isQuanX() || this.isLoon()) && $done(t) } }(t, e) }
