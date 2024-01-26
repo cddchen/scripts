290ae822b5d7989efb7098b59871d85440ece1db
陈东 <31310935+cddchen@users.noreply.github.com>
2024-01-26 20:37:25 +0800

    Update zilch.js

--- zilch.js
+++ zilch.js
@@ -1,6 +1,7 @@
 var objc = JSON.parse($response.body);
 
-objc.data.permissions.push("CONTACT_ADDRESS")
+// objc.data.permissions.push("CONTACT_ADDRESS")
+objc.data.permissions.pop()
 
 objc = JSON.stringify(objc)
 console.log(objc)
