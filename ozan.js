a7bcd15109146fb38ba10f5d3228533e379f48f7
陈东 <31310935+cddchen@users.noreply.github.com>
2024-03-12 12:07:05 +0800

    Update ozan.js

--- ozan.js
+++ ozan.js
@@ -1,8 +1,9 @@
 var objc = JSON.parse($response.body);
 
 if (objc?.accountHolderUserAssignments?.kycStatus)
-    objc?.accountHolderUserAssignments?.kycStatus = "KYC_LEVEL_3"
-objc?.kycStatus = "KYC_LEVEL_3"
+    objc.accountHolderUserAssignments.kycStatus = "KYC_LEVEL_3"
+if (objc?.kycStatus)
+    objc.kycStatus = "KYC_LEVEL_3"
 
 objc = JSON.stringify(objc)
 console.log(objc)
