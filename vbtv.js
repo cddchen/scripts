b65d6085cc07c198bbdcf2db85501f67c1cdefcf
陈东 <31310935+cddchen@users.noreply.github.com>
2024-04-17 13:59:00 +0800

    Update vbtv.js

--- vbtv.js
+++ vbtv.js
@@ -1,6 +1,6 @@
 var objc = JSON.parse($response.body);
 objc[0].license.contentDownload.permission = "ALLOWED"
-objc[0].license.type = "PREMIUM"
+objc[0].license.type = "STANDARD"
 objc[0].name = "MONTHLY PREMIUM"
 
 objc[3].status = "ACTIVE"
