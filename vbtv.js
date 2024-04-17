8f00cf6537f78ac492c43fde1d6f3257f5016d2b
陈东 <31310935+cddchen@users.noreply.github.com>
2024-04-17 14:26:45 +0800

    Update vbtv.js

--- vbtv.js
+++ vbtv.js
@@ -1,9 +1,14 @@
 var objc = JSON.parse($response.body);
-objc[0].status = "INACTIVE"
-objc[0].licenceStatus.status = "INACTIVE"
 
-objc[3].status = "ACTIVE"
-objc[3].licenceStatus.status = "ACTIVE"
+if (Array.isArray(objc)) {
+  objc[0].status = "INACTIVE"
+  objc[0].licenceStatus.status = "INACTIVE"
+  objc[3].status = "ACTIVE"
+  objc[3].licenceStatus.status = "ACTIVE"
+}
+else {
+  objc.accessLevel = "GRANTED"
+}
 
 objc = JSON.stringify(objc)
 console.log(objc)
