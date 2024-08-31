2971fb03bc15c80d74b010cdc430af8792257b69
陈东 <31310935+cddchen@users.noreply.github.com>
2024-08-31 16:06:36 +0800

    Create peakwatch.js

---
+++ peakwatch.js
@@ -0,0 +1,19 @@
+var objc = JSON.parse($response.body);
+
+if (objc.features) {
+  objc.features = Object.fromEntries(
+    Object.entries(objc.features).map(([name]) => {
+      return [name, true];
+    })
+  );
+}
+
+if (objc.content) {
+  objc.content.isSubscriber = true
+  objc.content.needForceUpgrade = false
+}
+
+console.log(JSON.stringify(objc));
+$done({
+  body: objc,
+});
