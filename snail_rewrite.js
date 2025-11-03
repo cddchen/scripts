3cf9dc519714287bbc1d222af439d327c7165c08
陈东 <31310935+cddchen@users.noreply.github.com>
2025-11-03 17:32:40 +0800

    Add JSON response structure in snail_rewrite.js

---
+++ snail_rewrite.js
@@ -0,0 +1,32 @@
+const objc = {
+    "code": 0,
+    "data": {
+        "balanceTypeId": "80005",
+        "balanceTypeName": "蜗牛nmsl",
+        "businessType": "CN",
+        "channel": [
+            "FREE_STORE",
+            "SPECIAL_EVENT",
+            "AGENT"
+        ],
+        "detailUrl": "",
+        "discountPrice": "12.0",
+        "effDate": 1707273354000,
+        "expDate": 1709308799000,
+        "feeIntroduce": "当月到账，每个月10G，限到账当月使用",
+        "iconUrl": "https://cloud.snailmobile.com/group1/M00/00/01/CgoBGWIgkc6AN50GAAQvPLVYfaw749.jpg",
+        "isOrderflag": "NOTORDER",
+        "label": "",
+        "linkBalanceTypeId": "510001",
+        "orderMaximun": "UNLIMITED",
+        "packageBusinessType": "TP",
+        "packageType": "MRP",
+        "price": "20.0",
+        "remark": " 当月到账，每个月10G，限到账当月使用",
+        "top": false
+    },
+    "msg": "success"
+};
+$done({
+  body: JSON.stringify(objc),
+});
